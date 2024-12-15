a637e8e2f478d23774fdb60159366f9c8a725013
didier-ndahimana <133503810+didier-ndahimana@users.noreply.github.com>
2024-12-15 08:52:33 +0200

    Add files via upload

---
+++ basic_operations.py
@@ -0,0 +1,11 @@
+number1=10
+number2=5
+sum=number1+number2
+diff=number1-number2
+multi=number1*number2
+print("Addition of ",number1,"and" ,number2 ,"is",sum)
+print("Substraction of ",number1,"and" ,number2 ,"is",diff)
+print("Multiplication of ",number1,"and", number2,"is",multi)
+
+
+
